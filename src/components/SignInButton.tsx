570aac8631c168bfdf8d40bb9500ba3b647a3da3
rexjoseph <josephrex96@gmail.com>
2023-04-07 07:24:33 -0500

    init commit

---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { FC } from 'react'
+
+interface SignInButtonProps {
+  
+}
+
+const SignInButton: FC<SignInButtonProps> = ({}) => {
+  const [isLoading, setIsLoading] = useState(<boolean></boolean>)
+  return <div>SignInButton</div>
+}
+
+export default SignInButton
