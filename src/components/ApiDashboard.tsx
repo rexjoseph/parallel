2e92787cef20c74eea20c11e04eb61f39c348329
rexjoseph <rexjoseph96@gmail.com>
2023-04-08 04:35:27 -0400

    begin writing dashboard

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -1,10 +1,38 @@
+import { authOptions } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { getServerSession } from 'next-auth'
+import { notFound } from 'next/navigation'
 import { FC } from 'react'
+import { formatDistance } from 'date-fns'
 
-interface ApiDashboardProps {
-  
-}
+const ApiDashboard = async () => {
+  const user = await getServerSession(authOptions)
+  if (!user) notFound()
 
-const ApiDashboard: FC<ApiDashboardProps> = ({}) => {
+  const apiKeys = await db.apiKey.findMany({
+    where: {
+      userId: user.user.id
+    }
+  })
+
+  const activeApiKey = apiKeys.find((apiKey) => apiKey.enabled)
+
+  if (!activeApiKey) notFound()
+
+  const userRequests = await db.apiRequest.findMany({
+    where: {
+      apiKeyId: {
+        in: apiKeys.map((key) => key.id),
+
+      }
+    }
+  })
+
+  const serializeableRequests = userRequests.map((req) => ({
+    ...req,
+    timestamp: formatDistance(new Date(req.timestamp), new Date())
+  }))
+
   return <div>ApiDashboard</div>
 }
 
